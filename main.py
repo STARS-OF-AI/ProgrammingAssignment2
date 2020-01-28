2207d020faefc2aad2d7bc2e1715ffde80ff3b9e
Rupika <rupika.d97@gmail.com>
2020-01-27 21:20:47 -0800

    - added scramble, parse_move, and main functions - added header comments - fixed spacing in puzzle.py - renamed 15puzzle.py to puzzle.py

---
+++ main.py
@@ -0,0 +1,92 @@
+"""
+Christopher Mendez & Rupika Dikkala
+Programming Assignment 2
+15 Puzzle
+CS 531 - AI
+February 3, 2020
+***********************************
+"""
+
+import puzzle as p
+import random as rand
+from copy import deepcopy
+
+
+def main():
+    shuffles = [10, 20, 30, 40, 50]
+    final = str([[0,  1,  2,  3],
+                 [4,  5,  6,  7],
+                 [8,  9, 10, 11],
+                 [12, 13, 14, 15]])
+
+    for i in shuffles:
+        print("\nSHUFFLE COUNT: {}".format(i))
+        for j in range(0, 10):
+            puzzle = scramble(i, final)
+            print(puzzle)
+            # p.recursive_best_first(puzzle, final)
+            # p.iterative_deepening_astar_h1(puzzle, final)
+            # p.iterative_deepening_astar_h2(puzzle, final)
+
+
+# function that randomly shuffles the
+# puzzle board, param = # of shuffles
+def scramble(shuffle, board):
+    # coordinates of the blank space
+    x, y = 0, 0
+    moves = []
+
+    # shuffles the board for param
+    # number of times
+    for m in range(0, shuffle):
+        # if valid move, add to moves array
+        if x > 0:
+            moves.append("up")
+        if x < 3:
+            moves.append("down")
+        if y > 0:
+            moves.append("left")
+        if y < 3:
+            moves.append("right")
+
+        # now pick a move to perform
+        if len(moves) > 1:
+            # pick random move from array
+            move = rand.choice(moves)
+            # catch shuffled board and new blank space coordinates
+            board, x, y = deepcopy(parse_move(move, board, x, y))
+        else:
+            board, x, y = deepcopy(parse_move(moves[0], board, x, y))
+
+        moves.clear()
+
+    return board
+
+
+# performs the move on the board
+def parse_move(move, board, x, y):
+    # parse the string into int
+    current_board = eval(board)
+
+    # translating the move on the board
+    if move == "up":
+        current_board[x][y], current_board[x - 1][y] = current_board[x - 1][y], current_board[x][y]  #move up
+        x = x - 1
+    if move == "down":
+        current_board[x][y], current_board[x + 1][y] = current_board[x + 1][y], current_board[x][y]   #move down
+        x = x + 1
+    if move == "left":
+        current_board[x][y], current_board[x][y - 1] = current_board[x][y - 1], current_board[x][y]   #move left
+        y = y - 1
+    if move == "right":
+        current_board[x][y], current_board[x][y + 1] = current_board[x][y + 1], current_board[x][y]   #move right
+        y = y + 1
+
+    # return shuffled board and new coordinates of
+    # the blank space
+    return str(current_board), x, y
+
+
+if __name__ == "__main__":
+    main()
+
--- puzzle.py
+++ puzzle.py
@@ -1,4 +1,11 @@
-
+"""
+Christopher Mendez & Rupika Dikkala
+Programming Assignment 2
+15 Puzzle
+CS 531 - AI
+February 3, 2020
+***********************************
+"""
 
 def recursive_best_first(start,end):
     #recursive best first, currently just a bfs
@@ -7,10 +14,10 @@ def recursive_best_first(start,end):
     expanded_nodes=0
     while front:
         i = 0
-        for j in range(1, len(front)):    
+        for j in range(1, len(front)):
             if len(front[i]) > len(front[j]):
                 i = j
-        path = front[i]         
+        path = front[i]
         front = front[:i] + front[i+1:]
         endnode = path[-1]
         if endnode in expanded: continue
@@ -27,8 +34,8 @@ def recursive_best_first(start,end):
     print_path(path)
 
 def iterative_deepening_astar_h1(start,end):
-#a*
-    front = [[heuristic_1(start), start]] 
+    #a*
+    front = [[heuristic_1(start), start]]
     expanded = []
     expanded_nodes=0
     while front:
@@ -44,7 +51,7 @@ def iterative_deepening_astar_h1(start,end):
         if endnode in expanded: continue
         for k in moves(endnode):
             if k in expanded: continue
-            newpath = [path[0] + heuristic_1(k) - heuristic_1(endnode)] + path[1:] + [k] 
+            newpath = [path[0] + heuristic_1(k) - heuristic_1(endnode)] + path[1:] + [k]
             front.append(newpath)
             expanded.append(endnode)
         expanded_nodes += 1
@@ -56,8 +63,8 @@ def iterative_deepening_astar_h1(start,end):
 
 
 def iterative_deepening_astar_h2(start,end):
-#a*
-    front = [[heuristic_2(start), start]] 
+    #a*
+    front = [[heuristic_2(start), start]]
     expanded = []
     expanded_nodes=0
     while front:
@@ -73,7 +80,7 @@ def iterative_deepening_astar_h2(start,end):
         if endnode in expanded: continue
         for k in moves(endnode):
             if k in expanded: continue
-            newpath = [path[0] + heuristic_2(k) - heuristic_2(endnode)] + path[1:] + [k] 
+            newpath = [path[0] + heuristic_2(k) - heuristic_2(endnode)] + path[1:] + [k]
             front.append(newpath)
             expanded.append(endnode)
         expanded_nodes += 1
@@ -84,39 +91,40 @@ def iterative_deepening_astar_h2(start,end):
     print_path(path)
 
 
-def moves(mat): 
-#returns all possible moves
-    output = []  
+def moves(mat):
+    #returns all possible moves
+    output = []
 
-    m = eval(mat)   
+    m = eval(mat)
+    print(m)
     i = 0
     while 0 not in m[i]: i += 1
     j = m[i].index(0); #blank space (zero)
 
-    if i > 0:                                   
-      m[i][j], m[i-1][j] = m[i-1][j], m[i][j];  #move up
-      output.append(str(m))
-      m[i][j], m[i-1][j] = m[i-1][j], m[i][j]; 
-      
-    if i < 3:                                   
-      m[i][j], m[i+1][j] = m[i+1][j], m[i][j]   #move down
-      output.append(str(m))
-      m[i][j], m[i+1][j] = m[i+1][j], m[i][j]
+    if i > 0:
+        m[i][j], m[i-1][j] = m[i-1][j], m[i][j];  #move up
+        output.append(str(m))
+        m[i][j], m[i-1][j] = m[i-1][j], m[i][j];
 
-    if j > 0:                                                      
-      m[i][j], m[i][j-1] = m[i][j-1], m[i][j]   #move left
-      output.append(str(m))
-      m[i][j], m[i][j-1] = m[i][j-1], m[i][j]
+    if i < 3:
+        m[i][j], m[i+1][j] = m[i+1][j], m[i][j]   #move down
+        output.append(str(m))
+        m[i][j], m[i+1][j] = m[i+1][j], m[i][j]
 
-    if j < 3:                                   
-      m[i][j], m[i][j+1] = m[i][j+1], m[i][j]   #move right
-      output.append(str(m))
-      m[i][j], m[i][j+1] = m[i][j+1], m[i][j]
+    if j > 0:
+        m[i][j], m[i][j-1] = m[i][j-1], m[i][j]   #move left
+        output.append(str(m))
+        m[i][j], m[i][j-1] = m[i][j-1], m[i][j]
+
+    if j < 3:
+        m[i][j], m[i][j+1] = m[i][j+1], m[i][j]   #move right
+        output.append(str(m))
+        m[i][j], m[i][j+1] = m[i][j+1], m[i][j]
 
     return output
 
 def heuristic_1(puzz):
- #   Counts the number of misplaced tiles
+    #   Counts the number of misplaced tiles
     misplaced = 0
     compare = 0
     m = eval(puzz)
@@ -128,9 +136,9 @@ def heuristic_1(puzz):
     return misplaced
 
 def heuristic_2(puzz):
- #   Manhattan distance 
+    #   Manhattan distance
     distance = 0
-    m = eval(puzz)          
+    m = eval(puzz)
     for i in range(4):
         for j in range(4):
             if m[i][j] == 0: continue
@@ -141,19 +149,4 @@ def print_path(path):
     for i in path:
         print(i)
 
-def main():
-    puzzle = str([[1, 2, 6, 3],
-                  [4, 9, 5, 7],
-                  [8, 13, 11, 15],
-                  [12, 14, 0, 10]])
 
-    end = str([[0,  1,  2,  3],
-               [4,  5,  6,  7],
-               [8,  9, 10, 11],
-               [12, 13, 14, 15]])
-    recursive_best_first(puzzle,end)
-    iterative_deepening_astar_h1(puzzle,end)
-    iterative_deepening_astar_h2(puzzle,end)
-  
-
-main()
