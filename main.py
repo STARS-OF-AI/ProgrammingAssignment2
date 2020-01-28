4096cfbfaf028139c287c914d79ec6bba6ea111b
Chris Mendez <mendezc@oregonstate.edu>
2020-01-28 15:24:19 -0800

    removing non recursive

--- main.py
+++ main.py
@@ -30,8 +30,6 @@ def main():
                 path = front[0]
                 p.RBFS_H1(front, final, path)
                 p.RBFS_H2(front, final, path)
-                #p.recursive_best_first_h1(puzzle, final)
-                #p.recursive_best_first_h2(puzzle, final)
                 p.iterative_deepening_astar_h1(puzzle, final)
                 p.iterative_deepening_astar_h2(puzzle, final)
 
